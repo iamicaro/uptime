2b7f6793e774e75bdef845280c4bc226223dab24
Ícaro Silva <iamicaro@outlook.com>
2018-01-20 13:31:06 -0200

    Atualização da ReadFile

--- src/test/java/core/ReadFile.java
+++ src/test/java/core/ReadFile.java
@@ -6,28 +6,44 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReadFile {
-	
+
 	private static PropertiesConfig propertiesConfig = new PropertiesConfig();
-	
-	public static BufferedReader load(String nameFile, String typeFile) throws IOException {
+	private BufferedReader reader;
+	private List<String> lines = new ArrayList<>();
+
+	public ReadFile(String nameFile, String typeFile) throws IOException {
+		this.reader = load(nameFile, typeFile);
+		lines = reader.lines().collect(Collectors.toList());
+	}
+
+	private static BufferedReader load(String nameFile, String typeFile) throws IOException {
 		final String path = propertiesConfig.getProperties().getProperty("app.dir.files") + nameFile + "." + typeFile.toLowerCase();
 		InputStream fis = new FileInputStream(new File(path));
-	
 		return new BufferedReader(new InputStreamReader(fis));
 	}
-	
+
+	public List<String> getValuePosition(int initial, int finaly) throws IOException {
+		List<String> values = new ArrayList<>();
+		lines.forEach(l -> values.add(l.substring(initial, finaly)));
+		return values;
+	}
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader reader = load("teste", "txt");
-		String line;
-		StringBuilder out = new StringBuilder();
-        while ((line = reader.readLine()) != null) {
-            out.append(line.substring(0, 23));
-            out.append("\n");
-        }
-        System.out.println(out.toString());
-        reader.close();
- 
+
+		ReadFile readFile = new ReadFile("teste", "txt");
+
+		for(String value : readFile.getValuePosition(50, 52)) {
+			System.out.println(value);
+		}
+		
+		for(String value : readFile.getValuePosition(0, 23)) {
+			System.out.println(value);
+		}
+
 	}
 }
